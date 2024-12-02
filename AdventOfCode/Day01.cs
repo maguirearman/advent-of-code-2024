6005e687463e360901ba9c8302171c338cf0a837
Maguire Arman <maguire.arman@modmed.com>
2024-12-02 11:07:50 -0500

    finished day 1 challenge!!!

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,8 +1,10 @@
-﻿namespace AdventOfCode;
+﻿using System.Collections.Immutable;
+
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day01()
     {
@@ -12,9 +14,70 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
+        //initialize arrays for each list
+        int[] leftList = new int[_input.Length];
+        int[] rightList = new int[_input.Length];
+        int totalDistance = 0;
 
-        return new("");
+        //parse and create lists
+        for (int i = 0; i < _input.Length; i++)
+        {
+            string currentLine = _input[i];
+            string[] parts = currentLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            leftList[i] = int.Parse(parts[0]);
+            rightList[i] = int.Parse(parts[1]);
+        }
+
+        //sort lists
+        Array.Sort(leftList);
+        Array.Sort(rightList);
+
+        //calculate distance and add to total
+        for (int i = 0; i < _input.Length; i++)
+        {
+            int distance = Math.Abs(leftList[i] - rightList[i]);
+            totalDistance += distance;
+        }
+
+
+        return new(totalDistance.ToString());
     }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2()
+    {
+        //initialize arrays for each list
+        int[] leftList = new int[_input.Length];
+        int[] rightList = new int[_input.Length];
+        int similarityScore = 0;
+
+        //parse and create lists
+        for (int i = 0; i < _input.Length; i++)
+        {
+            string currentLine = _input[i];
+            string[] parts = currentLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            leftList[i] = int.Parse(parts[0]);
+            rightList[i] = int.Parse(parts[1]);
+        }
+
+        //sort lists
+        Array.Sort(leftList);
+        Array.Sort(rightList);
+
+        int leftNumber = 0;
+        for (int i = 0; i < _input.Length; i++)
+        {
+            leftNumber = leftList[i];
+            int leftNumberCount = 0;
+            for (int j = 0; j < _input.Length; j++)
+            {
+                if (rightList[j] == leftNumber)
+                {
+                    leftNumberCount++;
+                }
+            }
+            similarityScore = similarityScore + leftNumber * leftNumberCount;
+        }
+
+        return new(similarityScore.ToString());
+    }
 }
